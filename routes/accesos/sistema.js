7fb69254853071cb0ea94757c3ddff0048bc0372
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-03 15:27:53 -0500

    Retorna IDs generados, pero no asociados con los temp

--- routes/accesos/sistema.js
+++ routes/accesos/sistema.js
@@ -23,25 +23,28 @@ router.post('/guardar', function(request, response, next) {
     //http://docs.sequelizejs.com/manual/tutorial/transactions.html
     return models.db.transaction(function (t) {
         var promises = [];
-        nuevos.forEach(function(nuevo) {
-             models.sistema.create({nombre: nuevo['nombre'], version: nuevo['version'], repositorio: nuevo['repositorio']}, {transaction: t});
+        eliminados.forEach(function(eliminado) {
+            models.sistema.destroy({where: {id: eliminado}}, {transaction: t});
         });
         editados.forEach(function(editado) {
             models.sistema.update({nombre: editado['nombre'], version: editado['version'], repositorio: editado['repositorio']}, {where: {id: editado['id']}}, {transaction: t});
         });
-        eliminados.forEach(function(eliminado) {
-            models.sistema.destroy({where: {ids: eliminado}}, {transaction: t});
-        });
-        return Promise.all(array_nuevos).then(function(promises_nuevos) {
-            promises_nuevos.forEach(function(promise_nuevo){
-
+        nuevos.forEach(function(nuevo) {
+            var newPromises = models.sistema.create({nombre: nuevo['nombre'], version: nuevo['version'], repositorio: nuevo['repositorio']}, {transaction: t});
+             promises.push(newPromises);
+         });
+        return Promise.all(promises).then(function(nuevos_promises) {
+            var promises = [];
+            nuevos_promises.forEach(function(promise){
+                promises.push(promise['id']);
             });
+            return Promise.all(promises);
         });
     }).then(function (result) {
         console.log("++++++++ 1. RESULT ++++++++");
         console.log(result);
-        response.send(JSON.stringify('result'));
         console.log("++++++++ 2. RESULT ++++++++");
+        response.send(JSON.stringify(result));
     }).catch(function (err) {
         console.log("++++++++ 1. ERR ++++++++");
         console.log(err);
